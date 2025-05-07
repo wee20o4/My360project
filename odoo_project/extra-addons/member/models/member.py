170e88054b5882d29d70dda2527adb390a051432
wee20o4 <nmcuong519@gmail.com>
2025-05-07 16:02:02 +0700

    Update with image

--- odoo_project/extra-addons/member/models/member.py
+++ odoo_project/extra-addons/member/models/member.py
@@ -1,11 +1,9 @@
 from odoo import fields, models
 
 class Member(models.Model):
-    _name = 'member.member'
+    _inherit = 'res.partner'
     _description = 'Member'
 
-    name = fields.Char(string='Name', required=True)
-    email = fields.Char(string='Email')
     image_1920 = fields.Image(string='Avatar', max_width=1920, max_height=1920)
     company_name = fields.Char(string='Company Name')
     industry_id = fields.Many2one('res.partner.industry', string='Industry')
